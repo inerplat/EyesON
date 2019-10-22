604b12d74a8252ed86d3c542a05e20066d90a76d
simkyuwon <tlarbvkf@naver.com>
2019-10-22 21:03:40 +0900

    Create bluetooth protocol (#15)
    * bluetooth protocol test
    
    통신 프로토콜을 사용한 아두이노와 스마트폰 통신 테스트
    
    * modifu bluetoothTest.ino
    
    1. 테스트용 Serial 출력 삭제
    2. 블루투스 송수신 함수화

--- Arduino/TEST/bluetoothTest/bluetoothTest/bluetoothTest.ino
+++ Arduino/TEST/bluetoothTest/bluetoothTest/bluetoothTest.ino
@@ -1,18 +1,73 @@
 #include <SoftwareSerial.h>
 
+#define DATA_REQUEST '!'
+#define EYES_DATA '@'
+#define NOT_FOUND '#'
+#define CONNECTION '$'
+#define END_OF_DATA '~'
+
 SoftwareSerial bluetooth(10, 11);//RX,TX
 
-unsigned char outputBuffer[100];
-unsigned char inputBuffer[100];
+char outputBuffer[100];
+char inputBuffer[100] = {1,};
+int outputBufferIdx = 0;
+int inputBufferIdx = 0;
+
+enum arduinostate{READY, SENDING, WAITING, PROCESSING, WARNING}arduinoState = READY;
+
+void bluetoothConnection(){//bluetooth connection successful
+  while(!bluetooth.available());
+  bluetooth.read();
+}
+
+void sendData(String data){//output buffer setting
+  int i;
+  arduinoState = SENDING;
+  for(i = 0;i < data.length();i++){
+    outputBuffer[i] = data[i];
+  }
+  outputBuffer[i] = END_OF_DATA;
+}
+
+void bluetoothReceive(){
+  inputBufferIdx = 1;
+  while(inputBuffer[inputBufferIdx - 1] != END_OF_DATA){
+    while(!bluetooth.available());
+      inputBuffer[inputBufferIdx++] = bluetooth.read();
+  }
+  inputBuffer[inputBufferIdx] = '\0';
+}
+
+void bluetoothSend(){
+  for(outputBufferIdx = 0; outputBuffer[outputBufferIdx] != END_OF_DATA; outputBufferIdx++){
+    bluetooth.write(outputBuffer[outputBufferIdx]);
+  }
+  bluetooth.write(END_OF_DATA);
+}
 
 void setup() {
   bluetooth.begin(9600);
   Serial.begin(9600);
+  
+  bluetoothConnection();
+  sendData((String)DATA_REQUEST);
+  Serial.println("connection");
 }
 
 void loop() {
-  if(Serial.available())
-    bluetooth.write(Serial.read());
-   if(bluetooth.available())
-    Serial.write(bluetooth.read());
+  switch(arduinoState){
+    case SENDING://arduino->phone
+      bluetoothSend();
+      arduinoState = WAITING;
+     break;
+    case WAITING://arduino<-phone
+      bluetoothReceive();
+      arduinoState = PROCESSING;
+    break;
+    case PROCESSING:
+      sendData((String)DATA_REQUEST);
+    break;
+    case WARNING:
+    break;
+  }
 }
