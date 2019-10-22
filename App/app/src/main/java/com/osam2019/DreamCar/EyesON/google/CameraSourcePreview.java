8e5fcee995838105422d50ae9f474e3dcc15873d
DH Kim <inerplat@gmail.com>
2019-10-22 20:51:16 +0900

    add modified content
    Apache License 2.0에 의거해 Google LLC의 저작물에 대한 수정사항을 명시함

--- App/app/src/main/java/com/osam2019/DreamCar/EyesON/google/CameraSourcePreview.java
+++ App/app/src/main/java/com/osam2019/DreamCar/EyesON/google/CameraSourcePreview.java
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// inerplat : onLayout() is changed : enlarge the screen to modify the ratio of the camera
 package com.osam2019.DreamCar.EyesON.google;
 
 import android.annotation.SuppressLint;
