3aaacf333b9e0742da68fe906e7b522583fff208
simkyuwon <tlarbvkf@naver.com>
2019-10-23 01:01:36 +0900

    add protocol
    데이터 처리 위치를 아두이노에서 스마트폰으로 옮기고 그에 따른 프로토콜 추가

--- Arduino/EyesON/EyesON.ino
+++ Arduino/EyesON/EyesON.ino
@@ -5,6 +5,8 @@
 #define NOT_FOUND '#'
 #define CONNECTION '$'
 #define END_OF_DATA '~'
+#define SLEEP_CHECK '%'
+#define SLEEP_STATE_REQUEST '^'
 #define buzzerPin 13
 #define buzzerDelay 100//ms
 #define stepDelay 3//ms
@@ -16,7 +18,13 @@ char inputBuffer[100] = {1,};
 int outputBufferIdx = 0;
 int inputBufferIdx = 0;
 
+float leftEye = 0, rightEye = 0;
+float leftEyeWeightedAvg = 0.5, rightEyeWeightedAvg = 0.5;
+const float lastDataWeight = 0.2;
+const float avgRefVal = 0.5;
+
 enum arduinostate{READY, SENDING, WAITING, PROCESSING, WARNING}arduinoState = READY;
+enum alarmstate{OFF = '0', ON = '1'}alarmState = OFF;
 
 volatile int timer0Count = 0;
 const int stepmoterPin[4] = {4,5,6,7} ;
@@ -66,19 +74,20 @@ void setup() {
   pinMode(buzzerPin, OUTPUT);
   for(int i = 0;i < 4;i++)
     pinMode(stepmoterPin[i], OUTPUT);
-    
+
   TCCR0A = 0x02;//CTC mode
   TCCR0B = 0x03;//Clock / 64;
   OCR0A = 250;//1ms=OCR0A*64/16000000hz
   TCNT0 = 0;//Timer/Counter Register
   TIMSK0 = 0x02;//OCIE0A enable
-  
+    
   bluetoothConnection();
-  sendData((String)DATA_REQUEST);
+  sendData((String)SLEEP_STATE_REQUEST);
   Serial.println("connection");
 }
 
 void loop() {
+  Serial.println(alarmState);
     switch(arduinoState){
     case SENDING://arduino->phone
       bluetoothSend();
@@ -89,20 +98,45 @@ void loop() {
       arduinoState = PROCESSING;
     break;
     case PROCESSING:
-      sendData((String)DATA_REQUEST);
-    break;
-    case WARNING:
+      switch(inputBuffer[1]){
+        case EYES_DATA:
+/*          rightEye = ((String)inputBuffer).substring(2,5).toFloat()/100;
+          leftEye = ((String)inputBuffer).substring(5,8).toFloat()/100;
+          if(rightEye < rightEyeWeightedAvg * avgRefVal || leftEye < leftEyeWeightedAvg * avgRefVal){
+          }
+          else{
+            rightEyeWeightedAvg = rightEye * lastDataWeight + rightEyeWeightedAvg * (1 - lastDataWeight);
+            leftEyeWeightedAvg = leftEye * lastDataWeight + leftEyeWeightedAvg * (1 - lastDataWeight);
+          }*/
+        break;
+        case NOT_FOUND:
+        break;
+        case SLEEP_CHECK:
+          if(inputBuffer[2] == '1') {
+            alarmState = ON;
+          }
+          else if(inputBuffer[2] == '0'){
+            alarmState = OFF;
+          }
+        break;
+      }
+      sendData((String)SLEEP_STATE_REQUEST);
     break;
   }
 }
 
 ISR(TIMER0_COMPA_vect){
   TCNT0 = 0;
-  if(timer0Count % buzzerDelay == 0){
-    digitalWrite(buzzerPin, !digitalRead(buzzerPin));
+  if(alarmState == ON){
+    if(timer0Count % buzzerDelay == 0){
+      digitalWrite(buzzerPin, !digitalRead(buzzerPin));
+    }
+    if(timer0Count % stepDelay == 0){  
+      stepMove();
+    }
   }
-  if(timer0Count % stepDelay == 0){  
-    stepMove();
+  else{
+    digitalWrite(buzzerPin, LOW);
   }
   if(timer0Count % (buzzerDelay * stepDelay) == 0){
     timer0Count = 0;
